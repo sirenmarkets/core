959c50aed2a17357f581fdbf9d01b496cc35b615
siren-tamer <james@tokensoft.io>
2021-12-16 13:58:44 -0500

    Add storage layout check scripts

---
+++ scripts/storage_layout.ts
@@ -0,0 +1,14 @@
+const hre = require("hardhat")
+
+async function main() {
+  await hre.storageLayout.export()
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
