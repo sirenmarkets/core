c2071c3505f82d0e91589b34daa47bb6a3b3a7c5
Adam Feather <dakramystic@sirenmarkets.com>
2022-03-16 10:41:45 -0700

    added base script framework

---
+++ scripts/deploy_automatic_claim_keeper.ts
@@ -0,0 +1,23 @@
+import { deploy_automatic_claim_keeper } from "./lib/deploy_automaitc_claim_chainlink_keeper"
+import { argumentError } from "./lib/helper"
+
+const PRICE_ORACLE_DATE_OFFSET = parseInt(process.env.PRICE_ORACLE_DATE_OFFSET)
+if (
+  Number.isNaN(PRICE_ORACLE_DATE_OFFSET) ||
+  PRICE_ORACLE_DATE_OFFSET == null
+) {
+  argumentError("PRICE_ORACLE_DATE_OFFSET")
+}
+
+async function main() {
+  await deploy_automatic_claim_keeper()
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
